4cc5474bca69fc8f348b4c75ec6827165dd94e5a
saivamsi <sai.vamsi@carbonplace.com>
2024-06-26 16:20:52 +0530

    replace in tmp file for entrypoint.sh script

--- packages/client/entrypoint.sh
+++ packages/client/entrypoint.sh
@@ -8,11 +8,16 @@ replace_var() {
     eval "value=\$$1"
     if [ -z "$value" ]; then
         echo "WARN: Undefined variable $1"
-        sed -i "s,%$1%,,g" $2
+        cp $2 /tmp/a.js
+        sed -i "s,%$1%,,g" /tmp/a.js
+        cat /tmp/a.js > $2
+        rm -rf /tmp/a.js
     else
         echo "Setting variable $1"
-        sed -i "s,%$1%,$value,g" $2 > tmp; cat tmp > $2; rm tmp
-        echo "deleted tmp"
+        cp $2 /tmp/a.js
+        sed -i "s,%$1%,$value,g" /tmp/a.js
+        cat /tmp/a.js > $2
+        rm -rf /tmp/a.js
     fi
 }
 
@@ -20,7 +25,7 @@ if [ "$@" = 'nginx-fe' ]; then
 
     # go through all JS files and replace %VAR_NAME% with VAR_NAME value from env variables
 
-    find /var/www/ -type f -name "*main.*.js" | while read filename; do
+    find /var/www/ -type f -name "*.js" | while read filename; do
         replace_var REACT_APP_MSAL_AUTHORITY $filename
         replace_var REACT_APP_MSAL_CLIENT_ID $filename
         replace_var REACT_APP_MSAL_REDIRECT_URI $filename
