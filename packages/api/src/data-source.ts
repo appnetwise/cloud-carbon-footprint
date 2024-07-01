95a0d7ba8e81ad0d1a2ab8e6700201692588f220
niteshk-anw <niteshk.anw@gmail.com>
2024-07-01 18:10:37 +0530

    added logger info for mongo uri and database

--- packages/api/src/data-source.ts
+++ packages/api/src/data-source.ts
@@ -1,20 +1,18 @@
 import 'reflect-metadata'
 import { DataSource } from 'typeorm'
-import { configLoader } from '@cloud-carbon-footprint/common'
+import { Logger, configLoader } from '@cloud-carbon-footprint/common'
 
-import * as dotenv from 'dotenv'
-
-dotenv.config()
-
-const { NODE_ENV } = process.env
+const serverLogger = new Logger('Data Source')
+serverLogger.info(`Mongo URI: ${configLoader()?.MONGODB.URI}`)
+serverLogger.info(`Mongo Database: ${configLoader()?.MONGODB.DATABASE}`)
 export const AppDataSource = new DataSource({
   type: 'mongodb',
   url: configLoader()?.MONGODB.URI,
   database: configLoader()?.MONGODB.DATABASE,
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  synchronize: NODE_ENV === 'dev' ? false : false,
-  logging: NODE_ENV === 'dev' ? false : false,
+  synchronize: false,
+  logging: false,
   entities: ['src/**/*.entity{.ts,.js}'],
   // migrations: [__dirname + '/migrations/*{.ts,.js}'],
   // subscribers: [__dirname + '/**/*.subscriber{.ts,.js}'],
