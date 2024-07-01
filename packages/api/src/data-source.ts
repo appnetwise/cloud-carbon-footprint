e7b6b47b2e56bfa2a327fde96a61841ed7e9ff29
niteshk-anw <niteshk.anw@gmail.com>
2024-07-01 17:32:34 +0530

    loading mongo uri and database from config

--- packages/api/src/data-source.ts
+++ packages/api/src/data-source.ts
@@ -1,16 +1,16 @@
 import 'reflect-metadata'
 import { DataSource } from 'typeorm'
+import { configLoader } from '@cloud-carbon-footprint/common'
 
 import * as dotenv from 'dotenv'
 
 dotenv.config()
 
-const { MONGODB_URI, MONGODB_DATABASE, NODE_ENV } = process.env
-
+const { NODE_ENV } = process.env
 export const AppDataSource = new DataSource({
   type: 'mongodb',
-  url: MONGODB_URI,
-  database: MONGODB_DATABASE,
+  url: configLoader()?.MONGODB.URI,
+  database: configLoader()?.MONGODB.DATABASE,
   useNewUrlParser: true,
   useUnifiedTopology: true,
   synchronize: NODE_ENV === 'dev' ? false : false,
