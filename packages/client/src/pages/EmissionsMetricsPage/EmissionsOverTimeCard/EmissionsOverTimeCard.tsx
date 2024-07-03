8d77c7886af7bbcfc6e20f92d80f6f4ba63510f8
Rashid Ali <rashid.ali@carbonplace.com>
2024-07-03 15:35:31 +0530

    added error handling to footprint Emissions Over Time component

--- packages/client/src/pages/EmissionsMetricsPage/EmissionsOverTimeCard/EmissionsOverTimeCard.tsx
+++ packages/client/src/pages/EmissionsMetricsPage/EmissionsOverTimeCard/EmissionsOverTimeCard.tsx
@@ -7,20 +7,27 @@ import { EstimationResult } from '@cloud-carbon-footprint/common'
 import NoDataMessage from '../../../common/NoDataMessage'
 import DashboardCard from '../../../layout/DashboardCard'
 import ApexLineChart from './ApexLineChart/ApexLineChart'
+import ErrorPage from 'src/layout/ErrorPage'
 
 type EmissionsOverTimeProps = {
   data: EstimationResult[]
+  error: Error
 }
 
 const EmissionsOverTimeCard: FunctionComponent<EmissionsOverTimeProps> = ({
   data,
-}): ReactElement =>
-  data.length ? (
+  error,
+}): ReactElement => {
+  if (error) {
+    return <ErrorPage></ErrorPage>
+  }
+  return data.length ? (
     <DashboardCard testId="cloudUsage">
       <ApexLineChart data={data} />
     </DashboardCard>
   ) : (
     <NoDataMessage isTop isBold title="Cloud Usage" loading={true} />
   )
+}
 
 export default EmissionsOverTimeCard
