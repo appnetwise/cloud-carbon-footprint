389f2d5e8f76d4e12cb8ffb2df8764e60beeee1c
Rashid Ali <rashid.ali@carbonplace.com>
2024-08-12 11:03:31 +0530

    script modified

--- docker/keycloak/theme/anw/keyclock_anw_theme.sh
+++ docker/keycloak/theme/anw/keyclock_anw_theme.sh
@@ -1,47 +1,21 @@
 #!/bin/bash
 
 # Set variables
-KEYCLOAK_CONTAINER_NAME="keycloak-keycloak_demo_using_postgres-1"
 KEYCLOAK_DIR="/opt/keycloak"
 CUSTOM_THEME_NAME="anw"
 
-# Function to execute Docker commands
-docker_exec() {
-    docker exec $KEYCLOAK_CONTAINER_NAME sh -c "$*"
-}
-
 # Check if the Keycloak directory exists
-if ! docker_exec "[ -d $KEYCLOAK_DIR ]"; then
-    echo "Error: $KEYCLOAK_DIR does not exist in the container."
+if [ ! -d "$KEYCLOAK_DIR" ]; then
+    echo "Error: $KEYCLOAK_DIR does not exist."
     exit 1
 fi
 
 # Create directory structure
-docker_exec "mkdir -p $KEYCLOAK_DIR/themes/$CUSTOM_THEME_NAME/{account,admin,login}/{resources/{css,img},messages}"
-
-# Function to copy local file to container
-copy_to_container() {
-    local src=$1
-    local dest=$2
-    if [ -f "$src" ]; then
-        docker cp "$src" "$KEYCLOAK_CONTAINER_NAME:$dest"
-    else
-        echo "Warning: $src not found. Skipping."
-    fi
-}
-
-# Copy custom resources
-copy_to_container "logo.svg" "$KEYCLOAK_DIR/themes/$CUSTOM_THEME_NAME/account/resources/logo.svg"
-copy_to_container "favicon.ico" "$KEYCLOAK_DIR/themes/$CUSTOM_THEME_NAME/account/resources/favicon.ico"
-copy_to_container "./logo.svg" "$KEYCLOAK_DIR/themes/$CUSTOM_THEME_NAME/admin/resources/logo.svg"
-copy_to_container "./favicon.ico" "$KEYCLOAK_DIR/themes/$CUSTOM_THEME_NAME/admin/resources/favicon.ico"
-copy_to_container "./favicon.ico" "$KEYCLOAK_DIR/themes/$CUSTOM_THEME_NAME/login/resources/img/favicon.ico"
-#copy_to_container "./keycloak-bg.png" "$KEYCLOAK_DIR/themes/$CUSTOM_THEME_NAME/login/resources/img/keycloak-bg.png"
-copy_to_container "./keycloak-logo-text.svg" "$KEYCLOAK_DIR/themes/$CUSTOM_THEME_NAME/login/resources/img/keycloak-logo-text.svg"
+mkdir -p "$KEYCLOAK_DIR/themes/$CUSTOM_THEME_NAME"/{account,admin,login}/{resources/{css,img},messages}
 
 # Create custom CSS files
 for theme in account admin login; do
-    docker_exec "cat << EOF > $KEYCLOAK_DIR/themes/$CUSTOM_THEME_NAME/$theme/resources/css/styles.css
+    cat << EOF > "$KEYCLOAK_DIR/themes/$CUSTOM_THEME_NAME/$theme/resources/css/styles.css"
 body {
     background: #f5f5f5;
 }
@@ -138,45 +112,36 @@ div#kc-header-wrapper {
 #kc-registration > span >a {
      color: #fff;
 } 
-
-EOF"
+EOF
 done
 
 # Create theme.properties files
 for theme in account admin login; do
-    docker_exec "cat << EOF > $KEYCLOAK_DIR/themes/$CUSTOM_THEME_NAME/$theme/theme.properties
+    cat << EOF > "$KEYCLOAK_DIR/themes/$CUSTOM_THEME_NAME/$theme/theme.properties"
 parent=keycloak
 import=common/keycloak
 styles=css/login.css css/styles.css
-
-EOF"
+EOF
 done
 
 # Create messages_en.properties file
-docker_exec "cat << EOF > $KEYCLOAK_DIR/themes/$CUSTOM_THEME_NAME/login/messages/messages_en.properties
+cat << EOF > "$KEYCLOAK_DIR/themes/$CUSTOM_THEME_NAME/login/messages/messages_en.properties"
 loginAccountTitle=Let us go
-EOF"
+EOF
 
 # Find kcadm.sh location
 KCADM_PATH="$KEYCLOAK_DIR/bin/kcadm.sh"
-if ! docker_exec "[ -f $KCADM_PATH ]"; then
+if [ ! -f "$KCADM_PATH" ]; then
     echo "Error: Unable to find kcadm.sh at $KCADM_PATH"
     exit 1
 fi
 
 # Print environment variables for debugging
-docker_exec "env"
-
+env
 
 # Update Keycloak configuration
+"$KCADM_PATH" config credentials --server 'https://localhost:8443' --realm master --user admin --password admin --truststore '/opt/keycloak/conf/truststore.jks'
 
-docker_exec "\"$KCADM_PATH\" config credentials --server 'https://localhost:8443' --realm master --user admin --password admin --truststore '/opt/keycloak/conf/truststore.jks'"
+"$KCADM_PATH" update realms/master -s loginTheme=$CUSTOM_THEME_NAME -s accountTheme=$CUSTOM_THEME_NAME -s adminTheme=$CUSTOM_THEME_NAME --truststore '/opt/keycloak/conf/truststore.jks'
 
-docker_exec "\"$KCADM_PATH\" update realms/master -s loginTheme=$CUSTOM_THEME_NAME -s accountTheme=$CUSTOM_THEME_NAME -s adminTheme=$CUSTOM_THEME_NAME --truststore '/opt/keycloak/conf/truststore.jks'"
-
-
-
-# Restart Keycloak container
-docker restart $KEYCLOAK_CONTAINER_NAME
-
-echo "Keycloak theme update completed. Please check your Keycloak instance."
+echo "Keycloak theme update completed. Please restart the Keycloak service to apply changes."
